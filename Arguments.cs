59d4b8d90319c33ae514439402095b7cdf5d8777
Maxim Popov <maxim.popov@skyboxlabs.com>
2017-05-04 14:11:26 -0700

    first commit

---
+++ Arguments.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace asmref
+{
+    public class Arguments
+    {
+        private static readonly Regex OptionRegex = new Regex(@"[\-\\/](\w{1})", RegexOptions.Compiled);
+
+        private static readonly Dictionary<string, Action<Arguments>> OptionActions = new Dictionary<string, Action<Arguments>>(StringComparer.OrdinalIgnoreCase)
+        {
+            {"u", a => a.IsUpward = true},
+            {"d", a => a.IsDownward = true},
+            {"v", a => a.IsVerboseOutput = true}
+        };
+
+        public string AssemblyOrFileName { get; private set; }
+
+        public bool IsUpward { get; private set; }
+
+        public bool IsDownward { get; private set; }
+
+        public bool IsVerboseOutput { get; private set; }
+
+        public string ErrorMessage { get; private set; }
+
+        private Arguments()
+        {
+        }
+
+        public static Arguments Parse(string[] args)
+        {
+            var arguments = new Arguments();
+
+            foreach (var arg in args)
+            {
+                var match = OptionRegex.Match(arg);
+                if (match.Success)
+                {
+                    var option = match.Groups[1].Value;
+                    Action<Arguments> action;
+                    if (OptionActions.TryGetValue(option, out action))
+                    {
+                        action(arguments);
+                    }
+                    else
+                    {
+                        return BuildFailedArguments($"Unexpected option: {arg}");
+                    }
+                }
+                else if (arguments.AssemblyOrFileName != null)
+                {
+                    return BuildFailedArguments($"Unexpected argument: {arg}");
+                }
+                else
+                {
+                    arguments.AssemblyOrFileName = arg;
+                }
+            }
+
+            if (arguments.AssemblyOrFileName == null)
+            {
+                return BuildFailedArguments("Please specify short assembly name or assembly file name.");
+            }
+
+            if (arguments.IsUpward && arguments.IsDownward)
+            {
+                return BuildFailedArguments(@"""-u"" and ""-d"" cannot be used together.");
+            }
+
+            if (!arguments.IsUpward && !arguments.IsDownward)
+            {
+                arguments.IsUpward = true;
+            }
+
+            return arguments;
+        }
+
+        public static string BuildHelpMessage()
+        {
+            return $@"
+Usage:
+
+{Assembly.GetEntryAssembly().GetModules()[0].Name.ToLower()} [-u|-d] [-v] <short assembly name>|<assembly file name>
+
+-u   : Show assemblies that reference the given assembly (upward links).
+-d   : Show assemblies that are referenced the given assembly (downward links).
+-v   : Verbose output. Display warning messages.
+
+""-u"" and ""-d"" cannot be used together. If both are omitted, the default is ""-u"".";
+        }
+
+        private static Arguments BuildFailedArguments(string errorMessage)
+        {
+            return new Arguments { ErrorMessage = errorMessage };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace asmref
+{
+    class Program
+    {
+        private static readonly Regex AssemblyExtensionRegex = new Regex(@".(dll)|(exe)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        static int Main(string[] args)
+        {
+            var writer = Console.Out;
+
+            var arguments = Arguments.Parse(args);
+            if (arguments.ErrorMessage != null)
+            {
+                writer.WriteLine();
+                writer.WriteLine(arguments.ErrorMessage);
+                writer.WriteLine(Arguments.BuildHelpMessage());
+                return -1;
+            }
+
+            int scannedCount = 0;
+            int foundCount = 0;
+
+            var results = new Dictionary<string, List<AssemblyName>>();
+            string shortAssemblyName = arguments.AssemblyOrFileName;
+
+            string rootPath = Directory.GetCurrentDirectory();
+
+            if (AssemblyExtensionRegex.IsMatch(arguments.AssemblyOrFileName))
+            {
+                var assembly = Load(Path.Combine(rootPath, arguments.AssemblyOrFileName), rootPath, writer, arguments.IsVerboseOutput);
+                if (assembly != null)
+                {
+                    shortAssemblyName = assembly.GetName().Name;
+                }
+            }
+
+            foreach (var fullAssemblyPath in Directory.EnumerateFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                var extension = Path.GetExtension(fullAssemblyPath);
+                if (extension == null || !AssemblyExtensionRegex.IsMatch(extension))
+                {
+                    continue;
+                }
+
+                var relativeAssemblyPath = Path.GetFileName(fullAssemblyPath);
+                var assembly = Load(fullAssemblyPath, relativeAssemblyPath, writer, arguments.IsVerboseOutput);
+
+                if (assembly == null)
+                {
+                    continue;
+                }
+
+                scannedCount++;
+
+                foreach (var referencedAssemblyName in assembly.GetReferencedAssemblies())
+                {
+                    if (string.Equals(referencedAssemblyName.Name, shortAssemblyName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        shortAssemblyName = referencedAssemblyName.Name;
+
+                        List<AssemblyName> assemblyNames;
+                        if (!results.TryGetValue(referencedAssemblyName.FullName, out assemblyNames))
+                        {
+                            assemblyNames = new List<AssemblyName>();
+                            results.Add(referencedAssemblyName.FullName, assemblyNames);
+                        }
+                        assemblyNames.Add(assembly.GetName());
+
+                        foundCount++;
+                    }
+                }
+            }
+
+            var referencedAssemblyNames = results.Keys.ToArray();
+            Array.Sort(referencedAssemblyNames);
+
+            foreach (var referencedAssemblyName in referencedAssemblyNames)
+            {
+                var foundAssemblyNames = results[referencedAssemblyName];
+
+                foundAssemblyNames.Sort((n1, n2) => string.CompareOrdinal(n1.FullName, n2.FullName));
+
+                writer.WriteLine();
+                foreach (var foundAssemblyName in foundAssemblyNames)
+                {
+                    writer.WriteLine(foundAssemblyName.FullName);
+                }
+                writer.WriteLine($"\t=> {referencedAssemblyName}");
+            }
+
+            writer.WriteLine();
+            writer.WriteLine($"Number of assemblies that reference '{shortAssemblyName ?? arguments.AssemblyOrFileName}': {foundCount}");
+            writer.WriteLine($"Number of assemblies scanned: {scannedCount}");
+
+            return 0;
+        }
+
+        private static Assembly Load(string fullAssemblyPath, string relativeAssemblyPath, TextWriter writer, bool isVerboseOutput)
+        {
+            try
+            {
+                return Assembly.ReflectionOnlyLoadFrom(fullAssemblyPath);
+            }
+            catch (Exception)
+            {
+                if (isVerboseOutput)
+                {
+                    writer.WriteLine($"\t!!! {relativeAssemblyPath} failed to load into reflection context (perhaps it's not an assembly?)");
+                }
+                return null;
+            }
+        }
+    }
+}
